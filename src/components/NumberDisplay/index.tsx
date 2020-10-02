601394f28310e13a3e82ef18261d528f8be5ca77
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2020-10-02 11:45:37 +0530

    Finished completing the Header UI

---
+++ src/components/NumberDisplay/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import "./NumberDisplay.scss";
+
+interface NumberDisplayProps {
+  value: number;
+}
+
+const NumberDisplay: FC<NumberDisplayProps> = ({ value }) => {
+  return (
+    <div className="NumberDisplay">{value.toString().padStart(3, "0")}</div>
+  );
+};
+export default NumberDisplay;
